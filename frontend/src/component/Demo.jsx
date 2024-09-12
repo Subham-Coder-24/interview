2e625327a453ade46f3c58467648d276224fe93d
subham swarup pradhan <subhamswaruppradhan@subhams-MacBook-Air.local>
2024-09-12 22:10:16 +0530

    ok

--- frontend/src/component/Demo.jsx
+++ frontend/src/component/Demo.jsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React from 'react'
 
-const Dictaphone = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
-
+const demo = () => {
   return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
-  );
-};
-export default Dictaphone;
+    <div>demo</div>
+  )
+}
+
+export default demo
